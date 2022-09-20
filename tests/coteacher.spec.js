6bbaa8774984a015ca0da22b146de077112d6e6b
ajeeteshpanda <ajeeteshpanda20@gmail.com>
2022-09-21 01:28:48 +0530

    conflict 1

--- tests/coteacher.spec.js
+++ tests/coteacher.spec.js
@@ -4,18 +4,7 @@ const loginPayload = {provider: "local", email: "ajeetesh.panda@bytelearn.ai", p
 let token;
 let chapters;
 
-test.beforeAll( async()=> 
-{
-    const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://devuserapi.bytelearn.ai/api/v1/user/login",
-    {data: (loginPayload)});
 
-    expect(loginResponse.ok()).toBeTruthy();
-    console.log(loginResponse.status())
-    const loginResponseJSON = await loginResponse.json();
-    token = loginResponseJSON.access_token;
-
-});
 
 test('chatbot07pc', async function({page})
 {
