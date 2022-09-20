24ed8fd80a463d52aa81b9a0245eb85d0f492b3a
ajeeteshpanda <ajeeteshpanda20@gmail.com>
2022-09-21 01:04:46 +0530

    commit from Stuff to develop sep20-2

--- tests/coteacher.spec.js
+++ tests/coteacher.spec.js
@@ -2,3 +2,17 @@ const {test, expect,request} = require('@playwright/test');
 const { timeout } = require('../playwright.config');
 const loginPayload = {provider: "local", email: "ajeetesh.panda@bytelearn.ai", password: "testing@qa"};
 let token;
+let chapters;
+
+test.beforeAll( async()=> 
+{
+    const apiContext = await request.newContext();
+    const loginResponse = await apiContext.post("https://devuserapi.bytelearn.ai/api/v1/user/login",
+    {data: (loginPayload)});
+
+    expect(loginResponse.ok()).toBeTruthy();
+    console.log(loginResponse.status())
+    const loginResponseJSON = await loginResponse.json();
+    token = loginResponseJSON.access_token;
+
+});
